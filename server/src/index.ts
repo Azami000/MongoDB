6a95a78203055f71373df68125517d101f4130aa
24LP0649 24LP0649 <24LP0649@G302-CC0104.local>
2024-10-23 11:26:49 +0800

    nodmailer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import cors from "cors";
 
 dotenv.config();
 
@@ -7,8 +9,43 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
+
+const emailSender = async (
+  sendemail: string,
+  subject: string,
+  html: any,
+  text?: string
+) => {
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "chaka770@gmail.com",
+      pass: "nllecuctsylaqokj",
+    },
+  });
+
+  const options = {
+    from: "chaka770@gmail.com",
+    to: [sendemail],
+    html: html,
+    subject: subject,
+  };
+
+  await transport.sendMail(options);
+};
 
 app.get("/", async (req, res) => {
+  await emailSender(
+    "nyamkasharaw@gmail.com",
+    "hi",
+    ` <div style="background-color: blueviolet; color: blanchedalmond">
+      <a href="https://www.youtube.com/"> <button>yu bnaaa</button></a>
+    </div>`
+  );
   res.status(200).send({ message: "Hi" });
 });
 
